bb2a7da9023be7eb7dfd3a508870f2e9a6e3f79c
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-27 18:42:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  randomPrice = Math.floor(Math.random() * 100) + 1;
+  var randomPrice = Math.floor(Math.random() * 100) + 1;
   var newItem = {
     itemName: item,
     itemPrice: randomPrice
